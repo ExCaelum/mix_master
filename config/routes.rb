831f5377f17bcc90064e7816ccc5767b473e7a96
Parker P <exxcaelum@gmail.com>
2016-05-31 09:23:42 -0600

    refactored, tests passing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
+# Rails.application.routes.draw do
+#   resources :artists do
+#     resources :songs, only: [:new, :create]
+#   end
+#
+#   resources :songs, only: [:show]
+#   resources :playlists, only: [:index, :new, :create, :show, :edit, :update]
+# end
+
 Rails.application.routes.draw do
   resources :artists do
     resources :songs, only: [:new, :create]
   end
 
   resources :songs, only: [:show]
+  resources :playlists
 end
--- spec/features/user_views_all_artists_spec.rb
+++ spec/features/user_views_all_artists_spec.rb
@@ -1,4 +1,3 @@
-
 require 'rails_helper'
 
 RSpec.feature "User views all artists" do
